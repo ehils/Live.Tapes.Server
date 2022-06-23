92eb5b4a277002089108e7304518680a6ddd7f62
Erich Hils <mrerichhils@gmail.com>
2022-06-23 17:04:19 -0400

    deploy commits

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn livetapes.wsgi --log-file -
--- livetapes/settings.py
+++ livetapes/settings.py
@@ -12,10 +12,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
